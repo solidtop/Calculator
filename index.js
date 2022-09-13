8299cc0e002f04888c4d4e055c7e1c0b3335cec5
Axel Asp <solidtop.asp@gmail.com>
2022-09-13 14:19:23 +0200

    Update 25

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const functionButtons = document.querySelectorAll('[data-function]');
 
 const formatter = new Intl.NumberFormat('se');
 
-const maxAmountOfNumbers = 14;
+const maxAmountOfNumbers = 8;
 let amountOfNumbers = 0;
 
+let numbersAdded = [];
 let numbers = [0];
+let result = 0;
 let operators = [];
 let previousIsOperator = false;
 let displayText = ['0'];
 let isDecimal = false;
 let decimalPos = 0; 
-let previousNumberAdded = 0;
 
 let clearOnInput = false;
 
@@ -28,42 +29,52 @@ numberButtons.forEach(button => { //Add numbers to calculation
         }
 
         let numberToAdd = parseInt(button.innerHTML);
+        numbersAdded.push(numberToAdd); //save added number for later
         let lastPos = numbers.length - 1;
 
         if (previousIsOperator) {
             numbers.push(numberToAdd);
-            displayText.push(numbers[numbers.length - 1]);
+            displayText.push(numberToAdd);
+            previousIsOperator = false;
         } else {  
             if (isDecimal) {
                 decimalPos ++;
                 numbers[lastPos] = numbers[lastPos] + numberToAdd / 10 ** decimalPos; 
-                displayText[displayText.length-1] = numbers[lastPos].toFixed(decimalPos);
+                displayText[displayText.length-1] = numbers[lastPos].toLocaleString('se-SV', {minimumFractionDigits: decimalPos});
             } else {
                 numbers[lastPos] = numbers[lastPos] * 10 + numberToAdd;
                 displayText[displayText.length-1] = formatter.format(numbers[lastPos]);
             }
         } 
         updateDisplay(); 
-        previousIsOperator = false;
-        previousNumberAdded = numberToAdd;
         console.log(numbers);
     });   
 });
 operatorButtons.forEach(button => { //Add operators to calculation
     button.addEventListener('click', function() {
         if (previousIsOperator) return;
-        if (clearOnInput) return;
+
+        if (clearOnInput) {
+            clear();
+            numbers[0] = result;
+            displayText[0] = numbers[0];
+            updateDisplay();
+        }
+        console.log(isDecimal);
 
         if (button.innerHTML === ',') { //Decimal operator
+            if (isDecimal) return;
             isDecimal = true;
+            updateDisplay(','); //insert "fake" comma
         } else {
             operators.push(button.innerHTML);
             console.log(button.innerHTML);
             displayText.push(button.innerHTML);
             updateDisplay();  
             previousIsOperator = true;
-            isDecimal = false;
+            isDecimal = false; //reset values
             decimalPos = 0;
+            amountOfNumbers = 0;
         }
     });   
 });
@@ -87,8 +98,9 @@ functionButtons.forEach(button => { //Handle function buttons
 
 function calculate() {
     if (operators.length < 1) return;
+    if (numbers.length < 2) numbers[1] = numbers[0]; //if only one number exists, calculate itself
 
-    let result = numbers[0];
+    result = numbers[0];
     let operator = '';
     for (let i = 1; i < numbers.length; i++) {
         operator = operators[i-1];
@@ -105,13 +117,13 @@ function calculate() {
     showResult(result);
 }
 
-function updateDisplay() {
+function updateDisplay(str2 = '') {
     const display = document.getElementById('input-display');
     let str = '';
     displayText.forEach(text => {
        str += text + ' ';  
     });
-    display.innerHTML = str;
+    display.innerHTML = str + str2;
 }
 function clear() {
     numbers = [0];
@@ -121,6 +133,7 @@ function clear() {
     isDecimal = false;
     decimalPos = 0;
     clearOnInput = false;
+    previousIsOperator = false;
 
     const display = document.getElementById('input-display');
     const resultDisplay = document.getElementById('result-display');
@@ -129,38 +142,37 @@ function clear() {
     console.log('Cleared');
 }
 function removePosition() {
-    if (numbers[0] === 0) return;
-
     if (previousIsOperator) {
         operators.pop();
         displayText.pop();
         previousIsOperator = false;
+        amountOfNumbers = maxAmountOfNumbers; //reset 
         console.log('Operator removed');
     } else {
+        if (numbers[0] === 0 && numbers.length === 1) return;
+
         let lastPos = numbers.length-1;
         if (numbers[lastPos] < 10) {
             if (numbers.length > 1) {
                 numbers.pop();
                 displayText.pop();
-                previousIsOperator = false;
+                previousIsOperator = true;
             } else {
                 clear();
                 previousIsOperator = false;    
             }
         } else {
-            if (!isDecimal) { 
-                numbers[lastPos] = Math.ceil(numbers[lastPos] / 10) - Math.ceil(previousNumberAdded / 10);
-                displayText[displayText.length-1] = formatter.format(numbers[lastPos]);
-            } else {
-
-            }
+            let previousNumberAdded = numbersAdded[numbersAdded.length-1];
+            numbers[lastPos] = Math.ceil(numbers[lastPos] / 10) - Math.ceil(previousNumberAdded / 10);
+            displayText[displayText.length-1] = formatter.format(numbers[lastPos]);
         }
+        numbersAdded.pop();
         amountOfNumbers --;
         console.log('Number removed');
     }
     updateDisplay(); 
 }
-function showResult(result) {
+function showResult() {
     const inputdisplay = document.getElementById('input-display');   
     const resultDisplay = document.getElementById('result-display');     
     inputdisplay.innerHTML += ' ='
